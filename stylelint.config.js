cc2b66b53a29cb064485ac848c5f0e165c63e5dc
DwiNugroho <adwinugroho1@gmail.com>
2022-02-07 04:08:56 +0700

    chore: setup linter and husky

---
+++ stylelint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  customSyntax: 'postcss-html',
+  extends: [
+    'stylelint-config-standard',
+    // 'stylelint-config-recommended-vue',
+    'stylelint-config-prettier',
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'function',
+          'if',
+          'else',
+          'each',
+          'include',
+          'mixin',
+          'for',
+          'extend',
+        ],
+      },
+    ],
+    'shorthand-property-no-redundant-values': null,
+  },
+};
